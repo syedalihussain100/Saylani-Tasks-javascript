740b10528e50ee500c4b4d82a8e5c9b51da3bc6e
Syed Muhemin Ali <59026436+syedalihussain100@users.noreply.github.com>
2020-06-07 17:12:13 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,175 @@
+///Alert//////////
+alert("Error! Please enter a valid Password");
+
+alert("Welcome to JS Land...\nHappy Coding");
+
+alert("Welcome to JS Land...");
+
+alert("Happy Coding!");
+
+alert("Hello... I can run JS through my web browser's console");
+
+////VARIABLES FOR STRINGS////////////
+var name = "Jhone Dhoe";
+alert(name);
+
+var age = "15 years old";
+alert(age);
+
+var course = "Certified Web Mobile Application Development";
+alert(course);
+
+var piza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(piza);
+
+var email = "My email address is example@example.com";
+alert(email);
+
+var book = "I am trying to learn off the Book A Smarter way to Learn Javascript";
+alert(book);
+
+// var html = "Yah! I can write HTML content through Javascript";
+// document.write(html);
+
+var draw = " ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ ";
+alert(draw);
+
+//////////////////VARIABLES FOR NUMBERS/////////////////
+var age = "15 years old";
+alert(age);
+
+var visited = "You have visited this site 14 times"
+alert(visited);
+
+// var birth = "My birth year is 1990 \n Data type of My decarled variable is number";
+// document.write(birth);
+
+
+// let store = " Jhon Dhoe ";
+// let stores = " ordered ";
+// let store1 = " 5 T-shirt on Closing Store ";
+
+// let personal = store + stores + store1;
+// document.write(personal);
+
+
+/////////////////////////VARIABLE NAMES: LEGAL & ILLEGAL///////////////
+
+// let fill = "Var names can only contain , numbers, $and _ . For example : $my_1stVariable";
+// let fill1 = "Var must beign with a letter, $ or _ . For example : $name, _name or name";
+// let fill2 = "Variable names are case sensitive";
+// let fill3 = "Variable names should not be JS Keyword";
+
+// document.write(fill);
+// document.write(fill1);
+// document.write(fill2);
+// document.write(fill3);
+
+////////////////Math Expression////////////
+
+// let math = "sum of 3 and 5 is 8";
+// document.write(math);
+
+// let expression = " Value after variable decalartion is ";
+// var woll = undefined;
+// document.write(expression + woll);
+
+// let val = " Value after increament is ";
+// var p = 5;
+// var p1 = ++p
+// document.write(val + p1)
+
+// let initial = " Initial value: ";
+// let process = 5;
+// document.write(initial + process);
+
+// let additions = " Value after addition is: ";
+// let add1 = 12 + 1;
+// document.write(add1);
+
+
+// let increaments = " Value after decreament is: ";
+// let inr = 13;
+// let innr = --inr;
+// document.write(increaments + innr);
+
+// let reminder = " The Reminder is: ";
+// let re1 = 0;
+// document.write(re1);
+
+// var total = " Total cost to buy a ticket to a movie is ";
+// var numt = 500 * 6;
+// var pkt = " PKR ";
+
+// var pts = total + numt + pkt;
+// document.write(pts);
+
+// document.write("Table of 4 <br/>");
+// document.write("4 x 1 = 4 <br/>");
+// document.write("4 x 2 = 8 <br/>");
+// document.write("4 x 3 = 12 <br/>");
+// document.write("4 x 4 = 16 <br/>");
+// document.write("4 x 5 = 20 <br/>");
+// document.write("4 x 6 = 24 <br/>");
+// document.write("4 x 7 = 28 <br/>");
+// document.write("4 x 8 = 32 <br/>");
+// document.write("4 x 9 = 36 <br/>");
+// document.write("4 x 10 = 40 <br/>");
+
+// var farenhit, celsuis
+
+// farenhit = prompt("Please Enter value is F", 0);
+// celsuis = (farenhit - 32) * (5 / 9)
+// document.write(farenhit + "F degree in celsius is " + celsuis + "c");
+
+
+// document.write("Shopping Card <br/>");
+// document.write("Price of item 1 is 650 <br/>");
+// document.write("Quantity of item 1 is 3 <br/>");
+// document.write("Price of item 2 is 100 <br/>");
+// document.write("quantity of item 2 is 7 <br/>");
+// document.write("Shipping Carges 100 <br/>");
+
+// let tol = 650 + 3 + 100 + 7 + 100;
+// document.write("Total cost of your order is" + tol);
+
+// document.write("Mark Sheet: 980 <br/>");
+// document.write("Mark Obtained: 804 <br/>");
+// let per = 980 % 804;
+// document.write("Percentage" + per);
+
+
+// var ust = 10 * 163.48;
+// var sau = 1 * 43.54;
+// var toy = 1634.8 + 43.54;
+
+
+// document.write("Currency in PKR <br/><br/>");
+// document.write("Total Curreny in PKR " + toy);
+
+// document.write("Current year: 2020 <br/><br/>");
+// document.write("Birth Year: 1999 <br/><br/>");
+// document.write("Your Age is: 22");
+
+// document.write("The Liftime Supply Calculator <br/><br/><br/>");
+// document.write("Favourite snack: chocalate ship<br/>");
+// document.write("current age: 15<br/>");
+// document.write("Estimated Maximum Age: 65<br/>");
+// document.write("Amount of snack per day:3<br/>");
+// document.write("You will need 1 chocalate chip to last you until the ripe old age of 65");
+
+document.write("Result:<br/>")
+document.write("The value of a is: 10<br/><br/>");
+document.write("...................................<br/><br/><br/>");
+
+document.write("The value of ++a is: 11<br/>")
+document.write("Now the value of a is:11<br/><br/><br/>");
+
+document.write("The value of ++a is:11<br/>");
+document.write("Now the value of a is:12<br/><br/><br/>");
+
+document.write("The value of --a is:11<br/>");
+document.write("Now the value of a is:11 <br/><br/><br/>");
+
+document.write("The value of --a is:11<br/>");
+document.write("Mow the value of a is: 10");
