4525d75eb2e91be6cc398e3d1a4d56f8d07b8ebe
syedalihussain100 <smuheminali@gmail.com>
2020-06-13 17:04:11 +0000

    Javascript assignmrnt changes

--- App.js
+++ App.js
@@ -1,45 +1,45 @@
 ///Alert//////////
-alert("Error! Please enter a valid Password");
+// alert("Error! Please enter a valid Password");
 
-alert("Welcome to JS Land...\nHappy Coding");
+// alert("Welcome to JS Land...\nHappy Coding");
 
-alert("Welcome to JS Land...");
+// alert("Welcome to JS Land...");
 
-alert("Happy Coding!");
+// alert("Happy Coding!");
 
-alert("Hello... I can run JS through my web browser's console");
+// alert("Hello... I can run JS through my web browser's console");
 
-////VARIABLES FOR STRINGS////////////
-var name = "Jhone Dhoe";
-alert(name);
+// ////VARIABLES FOR STRINGS////////////
+// var name = "Jhone Dhoe";
+// alert(name);
 
-var age = "15 years old";
-alert(age);
+// var age = "15 years old";
+// alert(age);
 
-var course = "Certified Web Mobile Application Development";
-alert(course);
+// var course = "Certified Web Mobile Application Development";
+// alert(course);
 
-var piza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
-alert(piza);
+// var piza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(piza);
 
-var email = "My email address is example@example.com";
-alert(email);
+// var email = "My email address is example@example.com";
+// alert(email);
 
-var book = "I am trying to learn off the Book A Smarter way to Learn Javascript";
-alert(book);
+// var book = "I am trying to learn off the Book A Smarter way to Learn Javascript";
+// alert(book);
 
 // var html = "Yah! I can write HTML content through Javascript";
 // document.write(html);
 
-var draw = " ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ ";
-alert(draw);
+// var draw = " ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ ";
+// alert(draw);
 
-//////////////////VARIABLES FOR NUMBERS/////////////////
-var age = "15 years old";
-alert(age);
+// //////////////////VARIABLES FOR NUMBERS/////////////////
+// var age = "15 years old";
+// alert(age);
 
-var visited = "You have visited this site 14 times"
-alert(visited);
+// var visited = "You have visited this site 14 times"
+// alert(visited);
 
 // var birth = "My birth year is 1990 \n Data type of My decarled variable is number";
 // document.write(birth);
@@ -151,25 +151,261 @@ alert(visited);
 // document.write("Birth Year: 1999 <br/><br/>");
 // document.write("Your Age is: 22");
 
-// document.write("The Liftime Supply Calculator <br/><br/><br/>");
-// document.write("Favourite snack: chocalate ship<br/>");
-// document.write("current age: 15<br/>");
-// document.write("Estimated Maximum Age: 65<br/>");
-// document.write("Amount of snack per day:3<br/>");
-// document.write("You will need 1 chocalate chip to last you until the ripe old age of 65");
 
-document.write("Result:<br/>")
-document.write("The value of a is: 10<br/><br/>");
-document.write("...................................<br/><br/><br/>");
+// document.write("The Lifetime Calculator <br/>");
+// var snack = "Chocalate Chip";
+// document.write("Favourite Snake: " + snack + "<br/>");
+// var currentage = 15;
+// document.write("Current age: " + currentage + "<br/>");
+// var maximumage = 65;
+// document.write("Established Maximum Age: " + maximumage + "<br/>");
+// var snackday = 3;
+// document.write("Amount of Snack Per Day: " + snackday + "<br/>");
+// var total1 = currentage + maximumage + snackday;
+// document.write("You will need " + total1 + " chocalate chip to last you until the ripe old age of " + maximumage + "<br/>")
 
-document.write("The value of ++a is: 11<br/>")
-document.write("Now the value of a is:11<br/><br/><br/>");
 
-document.write("The value of ++a is:11<br/>");
-document.write("Now the value of a is:12<br/><br/><br/>");
 
-document.write("The value of --a is:11<br/>");
-document.write("Now the value of a is:11 <br/><br/><br/>");
 
-document.write("The value of --a is:11<br/>");
-document.write("Mow the value of a is: 10");
+
+/////////////////////////// Tasks 6 to 9////////////////////////////
+
+
+// document.write("Result:<br/>")
+// document.write("The value of a is: 10<br/><br/>");
+// document.write("...................................<br/><br/><br/>");
+
+// document.write("The value of ++a is: 11<br/>")
+// document.write("Now the value of a is:11<br/><br/><br/>");
+
+// document.write("The value of ++a is:11<br/>");
+// document.write("Now the value of a is:12<br/><br/><br/>");
+
+// document.write("The value of --a is:11<br/>");
+// document.write("Now the value of a is:11 <br/><br/><br/>");
+
+// document.write("The value of --a is:11<br/>");
+// document.write("Mow the value of a is: 10");
+
+// var a = 2,
+//     b = 1;
+// var results = --a - --b + ++b + b--
+//     console.log(results);
+
+// var result1 = --a;
+// var result2 = --a - --b;
+// var result3 = --a - --b + ++b;
+// var result4 = --a - --b + ++b + b--;
+
+// console.log(result1, result2, result3, result4);
+
+// document.write("a is " + result3 + "<br/>");
+// document.write("b is " + result4 + "<br/>");
+
+// var aresult = result3 + result4;
+// document.write("result is " + aresult);
+
+
+
+// function createTable(objectArray, fields, fieldTitles) {
+//     let body = document.getElementsByTagName('body')[0];
+//     let tbl = document.createElement('table');
+//     let thead = document.createElement('thead');
+//     let thr = document.createElement('tr');
+//     fieldTitles.forEach((fieldTitle) => {
+//         let th = document.createElement('th');
+//         th.appendChild(document.createTextNode(fieldTitle));
+//         thr.appendChild(th);
+//     });
+//     thead.appendChild(thr);
+//     tbl.appendChild(thead);
+
+//     let tbdy = document.createElement('tbody');
+//     let tr = document.createElement('tr');
+//     objectArray.forEach((object) => {
+//         let tr = document.createElement('tr');
+//         fields.forEach((field) => {
+//             var td = document.createElement('td');
+//             td.appendChild(document.createTextNode(object[field]));
+//             tr.appendChild(td);
+//         });
+//         tbdy.appendChild(tr);
+//     });
+//     tbl.appendChild(tbdy);
+//     body.appendChild(tbl)
+//     return tbl;
+// }
+
+
+// createTable([
+//     { Subject: '', TotalMarks: '', ObtainedMarks: "", Percentage: "" },
+//     { Subject: 'Total Marks', TotalMarks: '100', ObtainedMarks: 54, Percentage: '54%' },
+//     { Subject: 'Obtained Marks', TotalMarks: '100', ObtainedMarks: 48, Percentage: '48%' },
+//     { Subject: "Percentage", TotalMarks: "100", ObtainedMarks: 54, Percentage: '54%' },
+//     { Subject: "", TotalMarks: 300, ObtainedMarks: "", Percentage: '1.56%' }
+
+// ], ['Subject', 'TotalMarks', "ObtainedMarks", "Percentage"], ['Subject', 'TotalMarks', "ObtainedMarks", "Percentage"]);
+
+///////////////////////////////9 to 11/////////////////////////////////
+// var enter = prompt("Enter your City");
+// if (enter === "karachi") {
+//     alert("Welcome to city of lights");
+// } else {
+//     alert("City Not Found");
+// }
+
+// var age = prompt("Enter your Age");
+// switch (age) {
+//     case 'male':
+//         alert("Good Morning Sir");
+//         break;
+//     case 'female':
+//         alert("Good Morning Ma’am.")
+//     default:
+
+// }
+
+// var fuel = prompt("Enter your Fuel");
+// if ("0.25litres") {
+//     alert("Please refill the fuel in your car");
+// }
+
+// var a = 4;
+// if (++a === 5) {
+//     alert("given condition for a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83) {
+//     alert("given condition of variable is true");
+// }
+
+// var c = 12;
+// if (c++ === 13) {
+//     alert("Condition 1 is true");
+// }
+
+// if (c === 12) {
+//     alert("Condition 2 is true");
+// }
+
+// if (++c < 14) {
+//     alert("Condition 3 is true");
+// }
+
+// if (c === 14) {
+//     alert("Condition 4 is true");
+// }
+
+// var materialcost = 20000;
+// var labourcost = 2000;
+// var totalcost = materialcost + labourcost;
+// if (totalcost === materialcost + labourcost) {
+//     alert("The Cost equal");
+// }
+
+// if (true) {
+//     alert("true")
+// }
+
+// if (false) {
+//     alert("false")
+// }
+
+// if ("car" < "cat") {
+//     alert("Car is Smallest than cat")
+// }
+
+// function myfunction() {
+//     document.getElementById("demo").innerHTML = "Your Traffic Data iss here"
+// }
+
+// var data = prompt("Please Enter Your Grade");
+// var b = "Markshett\nTotalMarks:300\nMarksObtained:219\nPercentage:73%\nGrade:B\nRemarks:You Need To Improve";
+// alert(b);
+
+
+// var check = prompt("Enter your Ranging number and secreat number");
+
+// var number1 = 1;
+// var number2 = 2;
+// var number3 = 3;
+// var number4 = 4;
+// var number5 = 5;
+// var number6 = 6;
+// var number = 7;
+// var number = 8;
+// var number = 9;
+// var number10 = 10;
+// var numbe1 = +1;
+// var numbe2 = +2;
+// var numbe3 = +3;
+// var numbe4 = +4;
+// var numbe5 = +5;
+// var numbe6 = +6;
+// var numbe7 = +7;
+// var numbe8 = +8;
+// var numbe9 = +9;
+// var numbe10 = +10;
+
+// if (number1) {
+//     alert("Bingo! Correct answer");
+// }
+
+// if (number2) {
+//     alert("Bingo! Correct answer");
+// }
+// if (number3) {
+//     alert("Bingo! Correct answer");
+// }
+// if (number4) {
+//     alert("Bingo! Correct answer");
+// }
+// if (number5) {
+//     alert("Bingo! Correct answer");
+// }
+// if (number6) {
+//     alert("Bingo! Correct answer");
+// }
+// if (number7) {
+//     alert("Bingo! Correct answer");
+// }
+// if (number8) {
+//     alert("Bingo! Correct answer");
+// }
+// if (number9) {
+//     alert("Bingo! Correct answer");
+// }
+// if (number10) {
+//     alert("Bingo! Correct answer");
+// }
+// if (numbe1) {
+//     alert("Close enough to the correct answer");
+// }
+// if (numbe2) {
+//     alert("Close enough to the correct answer");
+// }
+// if (numbe4) {
+//     alert("Close enough to the correct answer");
+// }
+// if (numbe5) {
+//     alert("Close enough to the correct answer");
+// }
+// if (numbe7) {
+//     alert("Close enough to the correct answer");
+// }
+// if (numbe8) {
+//     alert("Close enough to the correct answer");
+// }
+// if (numbe9) {
+//     alert("Close enough to the correct answer");
+// }
+// if (numbe10) {
+//     alert("Close enough to the correct answer");
+// }
+
+var calculator = prompt("Calculator number")
+var divide = 3 / 3;
+if (divide) {
+    alert(divide);
+} else(error);
